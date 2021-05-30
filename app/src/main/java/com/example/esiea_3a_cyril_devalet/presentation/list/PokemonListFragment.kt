e493331d47af72a97239ab27d02a72b928dc4ae2
Cyril-Devalet <cyril.devalet@gmail.com>
2021-05-31 00:43:14 +0200

    Add listener in PokemonAdapter for onClick

--- app/src/main/java/com/example/esiea_3a_cyril_devalet/presentation/list/PokemonListFragment.kt
+++ app/src/main/java/com/example/esiea_3a_cyril_devalet/presentation/list/PokemonListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.esiea_3a_cyril_devalet.R
@@ -24,7 +25,7 @@ class PokemonListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
 
-    private val adapter = PokemonAdapter(listOf())
+    private val adapter = PokemonAdapter(listOf(), ::onClickedPokemon)
 
     private val layoutManager = LinearLayoutManager(context)
 
@@ -67,5 +68,9 @@ class PokemonListFragment : Fragment() {
         })
 
 
+    }
+
+    private fun onClickedPokemon(pokemon: Pokemon) {
+        findNavController().navigate(R.id.navigateToPokemonDetailFragment)
     }
 }
